0b0988914612f700e57037746d49b7cab6a84d09
swapnilbhojane4 <swapnilbhojane4@gmail.com>
2024-10-05 00:30:42 +0530

    initial

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
+            <div className='flex flex-col gap-4'>
+
+                <p>IT'S TIME TO GET</p>
+                <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>JIM<span className='text-blue-400'>Fit</span></h1>
+            </div>
+        </div>
+    )
+}
