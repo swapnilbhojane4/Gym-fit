afd9b6161715b9b47606370a5fe3b103f0d33197
swapnilbhojane4 <swapnilbhojane4@gmail.com>
2024-10-08 15:11:48 +0530

    explore-programs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Hero from './components/Hero'
 import Workout from './components/Workout'
 import Generator from './components/Generator'
+import Programs from './components/Programs/Programs'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <main className='min-h-screen flex flex-col bg-gradient-to-r
     from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
       <Hero />
+      <Programs />
       <Generator />
       <Workout />
     </main>
--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
 
-export default function Generator () {
+export default function Generator() {
   return (
-    <div>Generator</div>
+    <SectionWrapper header={"generate your workout"}
+    title={['It\'s','Huge ','o\'Clock']}>
+
+    </SectionWrapper>
   )
 }
